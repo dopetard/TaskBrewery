57483af8b526e29edd95234fb226c5403a130fa9
ankur <ankur5126@icloud.com>
2017-07-13 18:30:40 +0800

    Initial Commit

---
+++ TaskBrewery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaskBrewery
+//
+//  Created by Ankur Kumar on 13/07/17.
+//  Copyright © 2017 Ankur Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
