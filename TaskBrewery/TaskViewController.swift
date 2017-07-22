e86d18ed2df33e2c3cf997db62a7b77f5141b88c
ankur <ankur5126@icloud.com>
2017-07-22 22:29:36 +0800

    added long tap to move tasks feature

--- TaskBrewery/TaskViewController.swift
+++ TaskBrewery/TaskViewController.swift
@@ -21,17 +21,19 @@ class TaskViewController: UIViewController, UITableViewDataSource, UITableViewDe
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
         
-
+        
         self.title = "Task Brewery"
         tableView.dataSource = self
         tableView.delegate = self
+        addLongPressGesture()
+
         
         effect = visualEffectView.effect
         visualEffectView.effect = nil
         
         addItemView.layer.cornerRadius = 5
+
         
         
         // Do any additional setup after loading the view.
@@ -111,7 +113,7 @@ final class alertController : UIAlertController {
     @IBAction func LogoutButton(_ sender: Any) {
         
         
-        let alertController = UIAlertController(title: "Log out", message: "Are you sure you want to logout?", preferredStyle: .alert)
+        let alertController = UIAlertController(title: "Logout", message: "Are you sure?", preferredStyle: .alert)
         
         // Create OK button
         let OKAction = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction!) in
@@ -148,6 +150,11 @@ final class alertController : UIAlertController {
     
     @IBOutlet var addItemView: UIView!
     var effect:UIVisualEffect!
+    var cellSnapshot: UIView?
+    var initialIndexPath: IndexPath?
+
+
+
     
     @IBAction func dismissPopup(_ sender: UIButton) {
         
@@ -177,6 +184,7 @@ final class alertController : UIAlertController {
         
     }
     
+    
     func getData() {
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         do {
@@ -193,8 +201,12 @@ final class alertController : UIAlertController {
         let delete = UITableViewRowAction(style: .default, title: "Delete") { (action:UITableViewRowAction, indexPath:IndexPath) in
             print("delete at:\(indexPath)")
             
+        
+            
             let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
             let task = self.tasks[indexPath.row]
+            
+
             context.delete(task)
             (UIApplication.shared.delegate as! AppDelegate).saveContext()
             do {
@@ -266,9 +278,92 @@ final class alertController : UIAlertController {
         return tasks.count
     }
     
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+}
+
+// code for drag and drop cells
+
+
+extension TaskViewController {
+    
+    func addLongPressGesture() {
+        let longpress = UILongPressGestureRecognizer(target: self, action: #selector(onLongPressGesture(sender: )))
+        tableView.addGestureRecognizer(longpress)
+    }
+    
+    func onLongPressGesture(sender: UILongPressGestureRecognizer) {
+        let locationInView = sender.location(in: tableView)
+        let indexPath = tableView.indexPathForRow(at: locationInView)
+        
+        if sender.state == .began {
+            if indexPath != nil {
+                initialIndexPath = indexPath
+                let cell = tableView.cellForRow(at: indexPath!)
+                cellSnapshot = snapshotOfCell(inputView: cell!)
+                var center = cell?.center
+                cellSnapshot?.center = center!
+                cellSnapshot?.alpha = 0.0
+                tableView.addSubview(cellSnapshot!)
+                
+                UIView.animate(withDuration: 0.25, animations: { () -> Void in
+                    center?.y = locationInView.y
+                    self.cellSnapshot?.center = center!
+                    self.cellSnapshot?.transform = (self.cellSnapshot?.transform.scaledBy(x: 1.05, y: 1.05))!
+                    self.cellSnapshot?.alpha = 0.99
+                    cell?.alpha = 0.0
+                }, completion: { (finished) -> Void in
+                    if finished {
+                        cell?.isHidden = true
+                    }
+                })
+            }
+        } else if sender.state == .changed {
+            var center = cellSnapshot?.center
+            center?.y = locationInView.y
+            cellSnapshot?.center = center!
+            
+            if ((indexPath != nil) && (indexPath != initialIndexPath)) {
+                swap(&tasks[indexPath!.row], &tasks[initialIndexPath!.row])
+                tableView.moveRow(at: initialIndexPath!, to: indexPath!)
+                initialIndexPath = indexPath
+            }
+        } else if sender.state == .ended {
+            let cell = tableView.cellForRow(at: initialIndexPath!)
+            cell?.isHidden = false
+            cell?.alpha = 0.0
+            UIView.animate(withDuration: 0.25, animations: { () -> Void in
+                self.cellSnapshot?.center = (cell?.center)!
+                self.cellSnapshot?.transform = CGAffineTransform.identity
+                self.cellSnapshot?.alpha = 0.0
+                cell?.alpha = 1.0
+            }, completion: { (finished) -> Void in
+                if finished {
+                    self.initialIndexPath = nil
+                    self.cellSnapshot?.removeFromSuperview()
+                    self.cellSnapshot = nil
+                }
+            })
+        }
+    }
+    
+    func snapshotOfCell(inputView: UIView) -> UIView {
+        UIGraphicsBeginImageContextWithOptions(inputView.bounds.size, false, 0.0)
+        inputView.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        let cellSnapshot = UIImageView(image: image)
+        cellSnapshot.layer.masksToBounds = false
+        cellSnapshot.layer.cornerRadius = 0.0
+        cellSnapshot.layer.shadowOffset = CGSize(width: -5.0, height: 0.0)
+        cellSnapshot.layer.shadowRadius = 5.0
+        cellSnapshot.layer.shadowOpacity = 0.4
+        return cellSnapshot
+    }
 }
